354d5030e54fb50a091947232d1b52cca1812532
plasma018 <nthuplasma018@gmail.com>
2019-04-22 12:54:15 +0800

    Create M1020.py

---
+++ plasma018/M1020.py
@@ -0,0 +1,29 @@
+
+class Solution:
+    def numEnclaves(self, A: List[List[int]]) -> int:
+      row = len(A) - 1
+      column = len(A[0]) - 1
+      _sum = 0
+
+      def help(A, i, j):
+        if i >= 0 and i < len(A) and j >= 0 and j < len(A[i]) and A[i][j] == 1: 
+          A[i][j] = 0
+          help(A , i-1, j)
+          help(A , i+1, j)
+          help(A , i, j+1)
+          help(A , i, j-1)
+
+      for i in range(len(A)):
+        help(A, i, 0)
+        help(A, i, column)
+
+      for j in range(len(A[0])):
+        help(A, 0, j)
+        help(A, row, j)
+
+      for i in range(1, row):
+        for j in range(1, column):
+          if A[i][j] == 1:
+            _sum += 1
+
+      return _sum
