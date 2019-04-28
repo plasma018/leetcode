3f8c6c2dac08c2a5a8f2f854dba5ab2a9ce7a99b
plasma018 <nthuplasma018@gmail.com>
2019-04-28 23:36:06 +0800

    Update E476.py

--- plasma018/E476.py
+++ plasma018/E476.py
@@ -1,3 +1,11 @@
 class Solution:
     def findComplement(self, num: int) -> int:
         return ((1 << len(bin(num)[2:])) - 1)^ num
+    
+    
+class Solution:
+    def findComplement(self, num: int) -> int:
+        _str = ''
+        for i in bin(num)[2:]:
+            _str += '0' if i is '1' else '1'
+        return int(_str, 2)
