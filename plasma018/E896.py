60dcc369c940f7f39c42ab8cb619483ead6c6785
plasma018 <nthuplasma018@gmail.com>
2019-05-09 17:38:02 +0800

    Update E896.py

--- plasma018/E896.py
+++ plasma018/E896.py
@@ -2,4 +2,25 @@ class Solution:
     def isMonotonic(self, A: List[int]) -> bool:
         B = sorted(A)
         return B == A or B[::-1] == A
+    
+ 
+class Solution:
+    def isMonotonic(self, A: List[int]) -> bool:
+        def isUper(A):
+            dummy = A[0]
+            for item in A:
+                if dummy > item:
+                    return False
+                dummy = item
+            return True
+        
+        def isLower(A):
+            dummy = A[0]
+            for item in A:
+                if dummy < item:
+                    return False
+                dummy = item
+            return True
+        return isUper(A) or isLower(A)   
+    
         
